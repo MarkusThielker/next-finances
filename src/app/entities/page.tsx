8dcc99d1f8a805e1e52ff9f1ebee071fc258cb60
Markus Thielker <mail.markus.thielker@gmail.com>
2024-03-10 12:39:10 +0100

    N-FIN-5: add entity page UI

--- src/app/entities/page.tsx
+++ src/app/entities/page.tsx
@@ -1,7 +1,33 @@
+import { prismaClient } from '@/prisma';
+import { getUser } from '@/auth';
+import React from 'react';
+import EntityPageClientContent from '@/components/EntityPageClientComponents';
+import entityCreateUpdate from '@/lib/actions/entityCreateUpdate';
+import entityDelete from '@/lib/actions/entityDelete';
+
 export default async function EntitiesPage() {
+
+    const user = await getUser();
+
+    const entities = await prismaClient.entity.findMany({
+        where: {
+            userId: user?.id,
+        },
+        orderBy: [
+            {
+                type: 'desc',
+            },
+            {
+                id: 'asc',
+            },
+        ],
+    });
+
     return (
-        <main className="flex items-center justify-center min-h-screen text-3xl">
-            Entities
-        </main>
+        <EntityPageClientContent
+            entities={entities}
+            onSubmit={entityCreateUpdate}
+            onDelete={entityDelete}
+            className="flex flex-col justify-center space-y-4 p-10"/>
     );
 }
