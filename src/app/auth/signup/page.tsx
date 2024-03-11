63c2ea56e9709c5dcb36dd4990b0a4a3115e4bac
Markus Thielker <mail.markus.thielker@gmail.com>
2024-03-11 03:55:47 +0100

    N-FIN-12: move authentication cards to top

--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -7,7 +7,7 @@ import { URL_SIGN_IN } from '@/lib/constants';
 
 export default async function SignUpPage() {
     return (
-        <Card className="w-full max-w-md">
+        <Card className="w-full max-w-md mt-12">
             <CardHeader>
                 <CardTitle>Sign up</CardTitle>
                 <CardDescription>Create a new account.</CardDescription>
