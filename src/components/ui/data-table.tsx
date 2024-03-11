6dd2fde2b5077c1ad516d3db2d881cef9c4fde6a
Markus Thielker <mail.markus.thielker@gmail.com>
2024-03-11 11:22:18 +0100

    N-FIN-20: add fixed width on actions column

--- src/components/ui/data-table.tsx
+++ src/components/ui/data-table.tsx
@@ -66,7 +66,8 @@ export function DataTable<TData, TValue>({
                                     data-state={row.getIsSelected() && 'selected'}
                                 >
                                     {row.getVisibleCells().map((cell) => (
-                                        <TableCell key={cell.id}>
+                                        <TableCell key={cell.id}
+                                                   className={cell.id.endsWith('actions') ? 'w-[120px]' : ''}>
                                             {flexRender(cell.column.columnDef.cell, cell.getContext())}
                                         </TableCell>
                                     ))}
